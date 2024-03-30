45c0a97bee20b13a6b5c202d161ec78ded72d7c7
Neffi42 <114996380+Neffi42@users.noreply.github.com>
2024-03-30 17:47:58 +0100

    removed forbidden keyword

--- ex00/src/megaphone.cpp
+++ ex00/src/megaphone.cpp
@@ -1,19 +1,17 @@
  #include <iostream>
  #include <string.h>
 
-using namespace std;
-
 int main(int argc, char const *argv[])
 {
-	string s = "";
+	std::string s = "";
 
 	if (argc == 1)
-		cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << endl;
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 	for (int i = 1; i < argc; i++)
 	{
 		s = argv[i];
 		transform(s.begin(), s.end(), s.begin(), ::toupper);
-		cout << s << endl;
+		std::cout << s << std::endl;
 	}
 	return 0;
 }
