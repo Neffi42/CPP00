48e70b92ee84ce34313de77ce228fd075aa083c4
Neffi42 <114996380+Neffi42@users.noreply.github.com>
2024-04-22 09:34:33 +0200

    updated sed

--- CPP_01/ex04/src/main.cpp
+++ CPP_01/ex04/src/main.cpp
@@ -1,22 +1,11 @@
 #include "Sed.hpp"
 
-bool	parseInput(std::string s) {
-	if (s.empty())
-	{
-		std::cerr << "Error: input cannot be empty" << std::endl;
-		return false;
-	}
-	return true;
-}
-
 bool	parsing(int ac, char **av, std::ifstream &in) {
 	if (ac != 4)
 	{
 		std::cerr << "Usage: ./sedisforlosers <filename> <toSearch> <toReplace>" << std::endl;
 		return false;
 	}
-	if (!parseInput(av[1]) || !parseInput(av[2]) || !parseInput(av[3]))
-		return false;
 	in.open(av[1]);
 	if (!in.good())
 	{
