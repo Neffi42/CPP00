b6d8afb31a041981417e855d8fff69e4e970cc64
Neffi42 <114996380+Neffi42@users.noreply.github.com>
2024-04-25 12:57:25 +0200

    03/00 done

--- CPP_03/ex00/src/main.cpp
+++ CPP_03/ex00/src/main.cpp
@@ -4,7 +4,16 @@ int main() {
 	ClapTrap clapTrap("Bob");
 
 	clapTrap.attack("nobody");
+	clapTrap.status();
 	clapTrap.takeDamage(5);
+	clapTrap.status();
 	clapTrap.beRepaired(7);
-
+	clapTrap.status();
+	for (int i = 0; i < 8; i++)
+		clapTrap.attack("nobody");
+	clapTrap.status();
+	clapTrap.attack("nobody");
+	clapTrap.takeDamage(66);
+	clapTrap.status();
+	clapTrap.beRepaired(7);
 }
