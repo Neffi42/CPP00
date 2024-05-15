05f97abe9907cf9312bac15a2f42aa5864813893
Neffi42 <114996380+Neffi42@users.noreply.github.com>
2024-05-15 14:40:50 +0200

    parsing done and conversion function in progress

--- CPP_06/ex00/src/ScalarConverter.cpp
+++ CPP_06/ex00/src/ScalarConverter.cpp
@@ -1,17 +1,26 @@
 #include "ScalarConverter.hpp"
+#include <cstdlib>
+#include <ctype.h>
 #include <cctype>
 #include <cstddef>
 #include <iostream>
 #include <limits>
+#include <sstream>
 #include <string>
 
-enum type {
-    typeChar = 1,
-    typeInt,
-    typeFloat,
-    typeDouble,
-    typePseudo
-};
+static int myStoi(const std::string &s) {
+    int x = 0;
+    std::istringstream(s) >> x;
+    return x;
+}
+
+static std::string itos(int x) {
+    std::stringstream ss;
+
+    ss << x;
+    std::string str = ss.str();
+    return str;
+}
 
 static bool isChar(const std::string &s) {
     if (s.length() != 1)
@@ -26,35 +35,79 @@ static bool isChar(const std::string &s) {
 }
 
 static bool isInt(const std::string &s) {
-    for (std::size_t i = s.at(0) == '-' ? 1 : 0; i < s.length(); i++) {
+    for (size_t i = s.at(0) == '-' ? 1 : 0; i < s.length(); i++) {
         if (!isdigit(s.at(i)))
             return false;
     }
-    return true;
+    return s == itos(myStoi(s));
 }
 
 static bool isFloat(const std::string &s) {
-    (void)s;
-    return true;
+    bool pointFound = false;
+    int mantissa = 0;
+
+    for (size_t i = 0; i < s.length(); i++) {
+        if (i == s.length() -1) {
+            if (s.at(i) != 'f')
+                return false;
+        }
+        else if (!isdigit(s.at(i))) {
+            if (pointFound == false && s.at(i) == '.')
+                pointFound = true;
+            else
+                return false;
+        }
+        else if (pointFound) {
+            mantissa++;
+            if (mantissa > 7)
+                return false;
+        }
+    }
+    return pointFound && mantissa > 0;
 }
 
 static bool isDouble(const std::string &s) {
-    (void)s;
-    return true;
+    bool pointFound = false;
+    int mantissa = 0;
+
+    for (size_t i = 0; i < s.length(); i++) {
+        if (!isdigit(s.at(i))) {
+            if (pointFound == false && s.at(i) == '.')
+                pointFound = true;
+            else
+                return false;
+        }
+        else if (pointFound) {
+            mantissa++;
+            if (mantissa > 15)
+                return false;
+        }
+    }
+    return pointFound && mantissa > 0;
+}
+
+static bool isPseudo(const std::string &s) {
+    if (s == "-inff" || s == "+inff" || s == "-inf" || s == "+inf" || s == "nan")
+        return true;
+    return false;
 }
 
 void ScalarConverter::convert(const std::string &s) {
     if (isChar(s)) {
-
+        std::cout << "Char type\n";
     }
     else if (isInt(s)) {
-
+        std::cout << "Int type\n";
     }
     else if (isFloat(s)) {
-
+        std::cout << "Float type\n";
     }
     else if (isDouble(s)) {
-
+        std::cout << "Double type\n";
+        std::cout << std::atof(s.c_str()) << "\n";
+    }
+    else if (isPseudo(s)) {
+        std::cout << "Pseudo type\n";
     }
     else {
         std::cout << "Undefined type\n";
