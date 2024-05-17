e8b9d6a9b17dfa58bdeedc3b2b5cea14208c3ea5
Neffi42 <114996380+Neffi42@users.noreply.github.com>
2024-05-17 14:31:32 +0200

    change to reference

--- CPP_07/ex00/include/whatever.hpp
+++ CPP_07/ex00/include/whatever.hpp
@@ -1,15 +1,15 @@
 #pragma once
 
-template <typename T> void swap(T &x, T &y) {
+template <typename T> void swap(T& x, T& y) {
     T tmp = x;
     x = y;
     y = tmp;
 }
 
-template <typename T> T max(T x, T y) {
+template <typename T> T max(const T& x, const T& y) {
     return x > y ? x : y;
 }
 
-template <typename T> T min(T x, T y) {
+template <typename T> T min(const T& x, const T& y) {
     return x < y ? x : y;
 }
