4fbe9abbcda945a5d411b57979a9c77345aa5a24
Neffi <114996380+Neffi42@users.noreply.github.com>
2024-05-23 21:56:59 +0200

    frist draft ok 09/02

--- CPP_09/ex02/src/PmergeMe.cpp
+++ CPP_09/ex02/src/PmergeMe.cpp
@@ -1,4 +1,5 @@
 #include "PmergeMe.hpp"
+#include <cmath>
 #include <cstddef>
 #include <ctime>
 #include <sys/time.h>
@@ -24,7 +25,7 @@ static void mergeVector(std::vector< std::pair<int, int> >& pairs, std::vector< 
     size_t i = 0, j = 0;
     while (i < arr1.size() && j < arr2.size()) {
         if(arr1.at(i).second <= arr2.at(j).second){
-            pairs.push_back(arr1.at(i));
+                pairs.push_back(arr1.at(i));
             i++;
         }
         else if(arr1.at(i).second > arr2.at(j).second){
@@ -56,31 +57,97 @@ static void mergeSortVector(std::vector< std::pair<int, int> >& pairs) {
     }
 }
 
-static void sortVector(std::vector<int>& v) {
-    std::vector< std::pair<int, int> > pairs;
-    pairs.resize(v.size() / 2 + 1);
+static std::vector<int> calculateGroupSizes(int totalElements) {
+    std::vector<int> sizes;
+    int i = 0, currentSize = 2, sum = 0;
 
+    while (sum + currentSize <= totalElements) {
+        sum += currentSize;
+        sizes.push_back(currentSize);
+        i++;
+        if (i < 2)
+            currentSize = sizes[i - 1];
+        else
+            currentSize = sizes[i - 2] * 2 + sizes[i - 1];
+    }
+
+    if (sum < totalElements) {
+        sizes.push_back(totalElements - sum);
+    }
+
+    return sizes;
+}
+
+static int binarySearch(const std::vector<int>& v, int element) {
+    int left = 0;
+    int right = v.size();
+
+    while (left < right) {
+        int mid = left + (right - left) / 2;
+        if (v[mid] < element) {
+            left = mid + 1;
+        }
+        else {
+            right = mid;
+        }
+    }
+
+    return left;
+}
+
+static void sortVector(std::vector<int>& v) {
+    bool isOdd = v.size() % 2;
+    std::pair<int, int> lastOdd;
+    std::vector< std::pair<int, int> > pairs;
+
+    pairs.resize(v.size() / 2);
     for (size_t i = 0; i < v.size(); i++) {
-        if (i % 2 == 0)
+        if (isOdd && i + 1 == v.size()) {
+            lastOdd.first = v[i];
+            lastOdd.second = 0;
+        }
+        else if (i % 2 == 0)
             pairs[i / 2].first = v[i];
         else
             pairs[i / 2].second = v[i];
-        if (i + 1 == v.size() && v.size() % 2)
-            pairs[i / 2].second = 0;
     }
 
     v.clear();
-
     for (std::vector< std::pair<int, int> >::iterator it = pairs.begin(); it != pairs.end(); it++) {
-        if (it->first > it->second)
+        if (it->first > it->second && it->second != 0)
             std::swap(it->first, it->second);
     }
     mergeSortVector(pairs);
     for (std::vector< std::pair<int, int> >::iterator it = pairs.begin(); it != pairs.end(); it++) {
-        if (it == pairs.begin() && it->first != 0)
+        if (it == pairs.begin())
             v.push_back(it->first);
         v.push_back(it->second);
     }
+    pairs.push_back(lastOdd);
+
+    int currentIndex = 1, totalElements = pairs.size() - 1;
+    std::vector< std::vector<int> > groups;
+    std::vector<int> sizes = calculateGroupSizes(totalElements);
+
+    for (std::vector<int>::iterator it = sizes.begin(); it != sizes.end(); it++) {
+        int& size = *it;
+        std::vector<int> group;
+        for (int i = 0; i < size; i++) {
+            group.push_back(pairs[currentIndex + size - 1 - i].first);
+        }
+        currentIndex += size;
+        groups.push_back(group);
+    }
+
+    for (std::vector<std::vector<int> >::iterator groupIt = groups.begin(); groupIt != groups.end(); ++groupIt) {
+        std::vector<int>& group = *groupIt;
+        for (std::vector<int>::iterator elemIt = group.begin(); elemIt != group.end(); ++elemIt) {
+            int y = *elemIt;
+            int pos = binarySearch(v, y);
+            v.insert(v.begin() + pos, y);
+        }
+    }
+
 }
 
 time_t PmergeMe::mergeInsertionSortVector(std::vector<int>& v) {
