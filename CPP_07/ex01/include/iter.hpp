468aee9e9dc03374d9c9ee75908575cadcd9c805
Neffi <114996380+Neffi42@users.noreply.github.com>
2024-05-30 11:17:04 +0200

    added const de mort

--- CPP_07/ex01/include/iter.hpp
+++ CPP_07/ex01/include/iter.hpp
@@ -10,7 +10,20 @@ void iter(T* array, const size_t &len, void (*f)(T&)) {
     }
 }
 
+template <typename T>
+void iter(const T* array, const size_t &len, void (*f)(const T&)) {
+    for (size_t i = 0; i < len; i++) {
+        f(array[i]);
+    }
+}
+
 template  <typename T>
 void print(T& el) {
+    std::cout << el << "\n";
+}
+
+
+template  <typename T>
+void print(const T& el) {
     std::cout << el << "\n";
 }
