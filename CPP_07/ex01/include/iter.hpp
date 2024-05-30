6cc3acb40389dfa650c716b24417571f4f4b809b
Neffi42 <114996380+Neffi42@users.noreply.github.com>
2024-05-30 19:23:42 +0200

    correcte 7/1

--- CPP_07/ex01/include/iter.hpp
+++ CPP_07/ex01/include/iter.hpp
@@ -3,25 +3,15 @@
 #include <cstddef>
 #include <iostream>
 
+
 template <typename T>
-void iter(T* array, const size_t &len, void (*f)(T&)) {
+void iter(T* array, const size_t &len, void (*f)(const T&)) {
+	if (array == NULL || f == NULL)
+		return ;
     for (size_t i = 0; i < len; i++) {
         f(array[i]);
     }
 }
-
-template <typename T>
-void iter(const T* array, const size_t &len, void (*f)(const T&)) {
-    for (size_t i = 0; i < len; i++) {
-        f(array[i]);
-    }
-}
-
-template  <typename T>
-void print(T& el) {
-    std::cout << el << "\n";
-}
-
 
 template  <typename T>
 void print(const T& el) {
